a550d1dfe5d71cdbb6e705a9a2166a3b3b658f55
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-05-30 13:30:27 +0200

    .

--- Arduino-for-school/2023.11.15_aedu_13.1.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(9600);
-  }
-void loop() {
-  for(int x=10; x<100; x=x+1) {
-    Serial.println(x);
-    x=x+2;
-    }
-  }
--- Arduino-for-school/9.2.ino
+++
@@ -1,9 +0,0 @@
-int d;
-void setup() {
-  pinMode(3,OUTPUT);
-  d=1000;
-}
-
-void loop() {
-  analogWrite(3,60);
-}
